e8a4d57d97484fac96690a91544f6cbc996fbad1
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2018-10-29 22:19:50 +0200

    add level

--- src/components/Channels/Channel/Channel.js
+++ src/components/Channels/Channel/Channel.js
@@ -8,7 +8,7 @@ const channel = ( props ) => {
 			<span className={classes.Channel_number}>{String(props.number).padStart(3, '0')}</span>
 			<img className={classes.Channel_image} src={`https://images.zattic.com/${props.channel.bestAvailableQuality.logo_black_84}`} alt="props.channel.title}" />
 			<span className={classes.Channel_title}>{props.channel.title}</span>
-			<span></span>
+			<span className={classes.Channel_level}>{props.channel.bestAvailableQuality.level}</span>
 		</div>
 	)
 };
