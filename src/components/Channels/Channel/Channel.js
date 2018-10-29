d8ff21f904718bbcab787ef7f33a65c6e3faa2ff
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2018-10-29 22:01:17 +0200

    add image

--- src/components/Channels/Channel/Channel.js
+++ src/components/Channels/Channel/Channel.js
@@ -6,6 +6,7 @@ const channel = ( props ) => {
 		<div className={classes.Channel+' col-6'}>
 
 			<span className={classes.Channel_number}>{String(props.number).padStart(3, '0')}</span>
+			<img className={classes.Channel_image} src={`https://images.zattic.com/${props.channel.bestAvailableQuality.logo_black_84}`} alt="props.channel.title}" />
 			<span></span>
 			<span></span>
 			{props.channel.title} - has quality {props.channel.bestAvailableQuality.level}
