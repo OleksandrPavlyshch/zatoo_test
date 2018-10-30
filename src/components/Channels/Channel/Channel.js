89a27e519f9300f691e36a1d8897752bef704904
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2018-10-30 12:07:33 +0200

    fix number

--- src/components/Channels/Channel/Channel.js
+++ src/components/Channels/Channel/Channel.js
@@ -5,7 +5,7 @@ const channel = ( props ) => {
 	return (
 		<div className={classes.Channel + ' col-6 ' + (props.isFocused ? classes.focused : '')}>
 
-			<span className={classes.Channel_number}>{String(props.number).padStart(3, '0')}</span>
+			<span className={classes.Channel_number}>{String(props.number + 1).padStart(3, '0')}</span>
 			<img className={classes.Channel_image} src={`https://images.zattic.com/${props.channel.bestAvailableQuality.logo_black_84}`} alt="props.channel.title}" />
 			<span className={classes.Channel_title}>{props.channel.title}</span>
 			<span className={classes.Channel_level}>{props.channel.bestAvailableQuality.level}</span>
