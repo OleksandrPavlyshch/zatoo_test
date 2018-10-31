836eefb79782470e9da464f764f3b715123b8a58
Oleksandr Pavlyshch <lokdawn80@gmail.com>
2018-11-01 00:02:24 +0200

    render favorites without star

--- src/components/Channel/Channel.js
+++ src/components/Channel/Channel.js
@@ -3,13 +3,13 @@ import classes from './Channel.css'
 
 const channel = ( props ) => {
 	return (
-		<div className={classes.Channel + ' col-6 ' + (props.isFocused ? classes.focused : '')}>
+		<div className={classes.Channel + ' ' + (props.isFocused ? classes.focused : '') + ' ' + (props.isFavorite ? 'col-12' : 'col-6')}>
 
 			<span className={classes.Channel_number}>{String(props.number + 1).padStart(3, '0')}</span>
 			<img className={classes.Channel_image} src={`https://images.zattic.com/${props.channel.bestAvailableQuality.logo_black_84}`} alt="props.channel.title}" />
 			<span className={classes.Channel_title}>{props.channel.title}</span>
 			<span className={classes.Channel_level}>{props.channel.bestAvailableQuality.level}</span>
-			<span className={classes.Channel_favorite}></span>
+			{!props.isFavorite && <span className={classes.Channel_favorite}></span>}
 		</div>
 	)
 };
--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Channel from '../Channel/Channel';
 
 const favorites = (props) => props.favorites.map( ( favoriteChannel, i ) => {
-	return <Channel number={i} key={favoriteChannel.id} channel={favoriteChannel} />;
+	return <Channel number={i} key={favoriteChannel.id} isFavorite={true} channel={favoriteChannel} />;
 });
 
 export default favorites;
